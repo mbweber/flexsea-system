b9c8133c21dc6e52eb39cd80254bfc82556026a5
Jean-Francois Duval <jfduval@aqra.ca>
2017-03-03 16:22:33 -0500

    packAndSend now adds the cmd field to a packet

--- src/flexsea_system.c
+++ src/flexsea_system.c
@@ -205,6 +205,7 @@ void packAndSend(uint8_t *shBuf, uint8_t cmd, uint8_t cmdType, uint16_t len, \
 		}
 
 		memcpy(p->packed, comm_str_1, numb);
+		p->cmd = (cmdType == CMD_READ) ? CMD_R(cmd) : CMD_W(cmd);
 		flexsea_send_serial_slave(p);
 	}
 	else
@@ -223,6 +224,7 @@ void packAndSend(uint8_t *shBuf, uint8_t cmd, uint8_t cmdType, uint16_t len, \
 		}
 
 		memcpy(p->packed, comm_str_1, numb);
+		p->cmd = (cmdType == CMD_READ) ? CMD_R(cmd) : CMD_W(cmd);
 		flexsea_send_serial_master(p);
 	}
 }
