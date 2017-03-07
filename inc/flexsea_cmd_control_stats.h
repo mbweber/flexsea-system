0141c1981029f31af9f1c5313523a5999c8cff93
David Weisdorf <weisdorf.david@gmail.com>
2017-03-07 10:25:38 -0500

    first pass at control stats command implementation

---
+++ inc/flexsea_cmd_control_stats.h
@@ -0,0 +1,47 @@
+/****************************************************************************
+	[Project] FlexSEA: Flexible & Scalable Electronics Architecture
+	[Sub-project] 'flexsea-system' System commands & functions
+	Copyright (C) 2016 Dephy, Inc. <http://dephy.com/>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************
+	[Lead developper] Jean-Francois (JF) Duval, jfduval at dephy dot com.
+	[Origin] Based on Jean-Francois Duval's work at the MIT Media Lab
+	Biomechatronics research group <http://biomech.media.mit.edu/>
+	[Contributors]
+*****************************************************************************
+	[This file] flexsea_cmd_control_stats: Exposing controller stats to the GUI
+*****************************************************************************/
+
+#ifndef INC_FLEXSEA_CMD_CONTROL_STATS_H
+#define INC_FLEXSEA_CMD_CONTROL_STATS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_flexsea_payload_ptr_control_stats(void);
+
+void tx_cmd_control_stats_r(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, uint16_t *len, \
+						int slave);
+
+void rx_cmd_control_stats_r(uint8_t *buf, uint8_t *info);
+void rx_cmd_control_stats_rr(uint8_t *buf, uint8_t *info);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif	//INC_FLEXSEA_CMD_CONTROL_STATS_H
